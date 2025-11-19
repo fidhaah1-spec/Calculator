369c155c4088ea15ce5b25a74a742c9fab58ee5c
fidhaah1-spec <fidhaah1@gmail.com>
2025-11-19 10:33:51 +0530

    feat: Add clearEntry function

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ function clearResult() {
     resultInput.value = '';
 }
 
+function clearEntry() {
+    let currentValue = resultInput.value;
+    let lastOperatorIndex = -1;
+    for (let i = currentValue.length - 1; i >= 0; i--) {
+        if (['+', '-', '*', '/', '%'].includes(currentValue[i])) {
+            lastOperatorIndex = i;
+            break;
+        }
+    }
+    if (lastOperatorIndex !== -1) {
+        resultInput.value = currentValue.slice(0, lastOperatorIndex + 1);
+    } else {
+        resultInput.value = '';
+    }
+}
+
 function deleteLast() {
     resultInput.value = resultInput.value.slice(0, -1);
 }
